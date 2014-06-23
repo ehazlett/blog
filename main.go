8a6fd46c74f020df41d8004ce40cc56534eee7dc
Evan Hazlett <ejhazlett@gmail.com>
2014-06-22 23:25:20 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+)
+
+var (
+	LISTEN_ADDR string
+)
+
+func init() {
+	flag.StringVar(&LISTEN_ADDR, "l", ":8080", "Listen address")
+
+}
+
+func main() {
+	flag.Parse()
+	panic(http.ListenAndServe(LISTEN_ADDR, http.FileServer(http.Dir("./content/public"))))
+
+}
